ca2465a1ef3b307d3897d0a658e3862cdbd69441
Nikema Prophet <nikema.prophet@popschools.com>
2019-08-29 20:22:35 -0700

    Fix port variable for deployment

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ app.get('/', (req, res) => res.send("Hello World!"))
 
 const port = process.env.PORT || 3000
 
-app.listen(3000, ()=> console.log('app listening on port ' + port))
+app.listen(port, ()=> console.log('app listening on port ' + port))
