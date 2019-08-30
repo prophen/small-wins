bc5dd6f8a7e31c5cb50c818dab6c1d494a74940c
Nikema Prophet <nikema.prophet@popschools.com>
2019-08-29 20:55:31 -0700

    Set up .env variables and create sms.js

---
+++ sms.js
@@ -0,0 +1,14 @@
+require('dotenv').config()
+
+const accountSid = process.env.ACCOUNT_SID
+const authToken = process.env.AUTH_TOKEN
+const client = require('twilio')(accountSid, authToken)
+
+client.messages
+  .create({
+    body: 'Hello World',
+    from: process.env.TWILIO_PHONE_NUMBER,
+    to: process.env.RECEIVING_PHONE_NUMBER
+
+  })
+  .then(message => console.log(message.sid))
