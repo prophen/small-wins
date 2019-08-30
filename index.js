0a922f34f8d907204276e04b7f78efe339e91001
Nikema Prophet <nikema.prophet@popschools.com>
2019-08-29 23:38:46 -0700

    Test sms route

--- index.js
+++ index.js
@@ -20,9 +20,22 @@ connection.once("open", () => {
 })
 
 const winsRouter = require("./routes/wins")
-const smsRouter = require("./routes/sms")
+// const smsRouter = require("./routes/sms")
 
 app.use("/wins", winsRouter)
-app.use("/sms", smsRouter)
+// app.use("/sms", smsRouter)
 
+require("dotenv").config()
+
+const accountSid = process.env.ACCOUNT_SID
+const authToken = process.env.AUTH_TOKEN
+const client = require("twilio")(accountSid, authToken)
+const MessagingResponse = require("twilio").twiml.MessagingResponse
+
+app.post("/sms", (req, res) => {
+  const twiml = new MessagingResponse()
+  twiml.message(`Good job. This is what you sent: ${req.body.Body}`)
+  res.writeHead(200, { "Content-Type": "text/xml" })
+  res.end(twiml.toString())
+})
 app.listen(port, () => console.log("app listening on port " + port))
